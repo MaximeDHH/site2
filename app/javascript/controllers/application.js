4a9f7d7253d1bdc6090bc0a65d8819ef2105b7a5
maxime do <maxime.beauge@gmail.com>
2025-04-23 14:39:20 +0200

    last

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,18 +8,6 @@ window.Stimulus   = application
 
 export { application }
 
-document.addEventListener("scroll", () => {
-  const header = document.querySelector("header");
-
-  // Si on a scrollé un peu (plus que 0px), on ajoute la classe 'scrolled'
-  if (window.scrollY > 0) {
-    header.classList.add("scrolled");
-    header.classList.remove("transparent");
-  } else {
-    header.classList.remove("scrolled");
-    header.classList.add("transparent");
-  }
-});
 
 const title = document.querySelector(".section-title");
 observer.observe(title);

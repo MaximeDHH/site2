7e0b8f9dfa2da56bb12dbc3f43d9da7d380e4d24
maxime do <maxime.beauge@gmail.com>
2025-04-23 12:05:53 +0200

    Initial commit - projet manucure vitrine

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,25 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  // Si on a scrollé un peu (plus que 0px), on ajoute la classe 'scrolled'
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+    header.classList.remove("transparent");
+  } else {
+    header.classList.remove("scrolled");
+    header.classList.add("transparent");
+  }
+});
+
+const title = document.querySelector(".section-title");
+observer.observe(title);
+
+// Dans le callback du même IntersectionObserver :
+if (entry.isIntersecting) {
+  section.classList.add("visible");
+  title.classList.add("visible");
+}
